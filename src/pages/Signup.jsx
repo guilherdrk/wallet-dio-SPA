44836e6785a2cf1270db1ca3a7181935db844b4a
guilherdrk <guibem20@gmail,com>
2025-11-12 09:12:12 -0300

    feat: como pegar o valor de inputs com React Hook Form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,8 +3,15 @@ import logo from '../assets/logo.png'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import { BiArrowBack } from "react-icons/bi"
+import { useForm } from 'react-hook-form'
 
 export default function Signup(){
+
+    const {register, handleSubmit} = useForm();
+
+    function handleSubmitForm(data){
+        console.log(data);
+    }
     
     return(
         <div className="flex flex-col items-center justify-around bg-zinc-900
@@ -14,11 +21,11 @@ export default function Signup(){
             <BiArrowBack className='text-white absolute top-3 left-3 text-3xl cursor-pointer hover:text-sky-700'/>
             </Link>
             <img src={logo} alt="" className='w-44' />
-            <form className='flex flex-col justify-center gap-4 w-full text-2xl'>
-                <Input type="text" placeholder="Full Name..."/>
-                <Input type="email" placeholder="E-mail"/>
-                <Input type="password" placeholder="Password"/>
-                <Input type="password" placeholder="Confirm Password"/>
+            <form onSubmit={handleSubmit(handleSubmitForm)} className='flex flex-col justify-center gap-4 w-full text-2xl'>
+                <Input type="text" placeholder="Full Name..." register={register} name="text"/>
+                <Input type="email" placeholder="E-mail" register={register} name="email"/>
+                <Input type="password" placeholder="Password" register={register} name="password"/>
+                <Input type="password" placeholder="Confirm Password" register={register} name="confirmPassword"/>
 
                 <Button type="submit" text="signup"/>
             </form>
